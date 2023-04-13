be71e9b62db53886e2e4227d0459fff2700bae63
Jaden Bertino <jaden@bertinofamily.com>
2023-04-13 16:37:24 -0700

    add signin logic to signin page

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, Link } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useSignIn } from '../../hooks/useSignIn'
 
 // styles
 import "./auth.css";
@@ -10,6 +11,7 @@ export default function SignIn() {
   const [password, setPassword] = useState("");
   const { user } = useAuthContext();
   const nav = useNavigate();
+  const { signin, error } = useSignIn()
 
   // redirect user to home page if signed in
   useEffect(() => {
@@ -17,26 +19,36 @@ export default function SignIn() {
       nav("/");
     }
   }, [user, nav]);
-  
+
+  function handleSubmit(e) {
+    e.target.preventDefault()
+    signin(email, password)
+  }
+
   return (
     <div className="container">
       <div className="row jcc aic">
         <div className="col">
-          <form className="auth">
-            <h2 className="header">welcome back</h2>
+          <form className="auth" onSubmit={handleSubmit}>
+            <h2 className="header">welcome back &nbsp;:&nbsp;&#41;</h2>
             <input
               type="email"
               value={email}
               placeholder="email"
-              onChange={e => setEmail(e.target.value)}
-              />
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <input
               type="password"
               placeholder="password"
               value={password}
-              onChange={e => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
             />
+            {error && <div className='error'>{error}</div>}
             <button className="btn">Sign In</button>
+            <span>
+              don't have an account yet?&nbsp;
+              <Link className="redirect" to="/signup">sign up here</Link>
+            </span>
           </form>
         </div>
       </div>
