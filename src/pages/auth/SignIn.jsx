42991291e368be868531974d1c4ec0186d19cdee
Jaden Bertino <jaden@bertinofamily.com>
2023-04-13 11:57:42 -0700

    add some more basic global css classes

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router"
+import { useAuthContext } from "../../hooks/useAuthContext"
+
+// styles
 import './auth.css'
 
 export default function SignIn() {
+  const { user } = useAuthContext()
+  const nav = useNavigate()
+
+  // redirect user to home page if signed in
+  useEffect(() => {
+    if (user) {
+      nav('/')
+    }
+  }, [user, nav])
   return (
-    <div className='signin'>
-      
-    </div>
+      <div className="container bg-dark">
+        <div className="row jcc aic">
+          <div className="col">
+            <div className="auth-content">
+
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,8 +1,23 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router"
+import { useAuthContext } from "../../hooks/useAuthContext"
+
+// styles
 import './auth.css'
 
 export default function SignUp() {
+  const { user } = useAuthContext()
+  const nav = useNavigate()
+
+  // redirect user to home page signed in
+  useEffect(() => {
+    if (user) {
+      nav('/')
+    }
+  }, [user, nav])
+
   return (
-    <div className='signup'>
+    <div className='auth'>
       
     </div>
   )
