ce736672825b96aa9f8938eeb79cc2763008f01a
Jaden Bertino <jaden@bertinofamily.com>
2023-04-13 20:27:37 -0700

    create basic 'select conversation' html + css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router"
 import { useAuthContext } from "../../hooks/useAuthContext"
 import { useSignOut } from '../../hooks/useSignOut'
@@ -9,6 +9,7 @@ export default function Home() {
   const { user } = useAuthContext()
   const nav = useNavigate()
   const { signout } = useSignOut()
+  const [conversations, setConversations] = useState([])
 
   // redirect user to sign in page if not signed in
   useEffect(() => {
@@ -19,10 +20,25 @@ export default function Home() {
 
   return (
     <div className='home'>
-      <div className="container vh-100 jc aic">
+      <div className="df vh-100 jcc aic">
         <nav>
           <button className="btn" onClick={signout}>sign out</button>
         </nav>
+        <div className="container">
+          <h1>Select A Conversation:</h1>
+          <div className="conversations">
+          {conversations.length ? 
+            conversations.map((conversation, i) => (
+              <div className="conversation" key={i}>
+                {/* insert fetched conversations here */}
+              </div>
+              )): null}
+            <div className="conversation">
+              <i className="fa-solid fa-plus"></i>
+              <p>New Conversation</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
