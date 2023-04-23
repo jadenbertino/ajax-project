b8b6937a7e4d107440b6a6c6b0d880c49cc12342
Jaden Bertino <jaden@bertinofamily.com>
2023-04-23 13:29:40 -0700

    change encrypt + decrypt functions into google cloud functions that use secrets

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const crypto = require('crypto');
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -8,18 +9,32 @@ const functions = require("firebase-functions");
 //   response.send("Hello from Firebase!");
 // });
 
-const crypto = require('crypto');
+exports.encryptText = functions
+  .runWith({ secrets: ['ENCRYPTION_KEY', 'ENCRYPTION_IV'] })
+  .onCall((plainText) => {
+    const keyHex = process.env.ENCRYPTION_KEY;
+    const ivHex = process.env.ENCRYPTION_IV;
+    const cipher = crypto.createCipheriv(
+      'aes-256-cbc',
+      Buffer.from(keyHex, 'hex'),
+      Buffer.from(ivHex, 'hex')
+    );
+    let encryptedText = cipher.update(plainText, 'utf8', 'hex');
+    encryptedText += cipher.final('hex');
+    return encryptedText;
+  });
 
-function encryptText(plainText, keyHex, ivHex) {
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(keyHex, 'hex'), Buffer.from(ivHex, 'hex'));
-  let encryptedText = cipher.update(plainText, 'utf8', 'hex');
-  encryptedText += cipher.final('hex');
-  return encryptedText
-}
-
-function decryptText(encryptedText, keyHex, ivHex) {
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(keyHex, 'hex'), Buffer.from(ivHex, 'hex'));
-  let clearText = decipher.update(encryptedText, 'hex', 'utf8');
-  clearText += decipher.final('utf8');
-  return clearText;
-}
+exports.decryptText = functions
+  .runWith({ secrets: ['ENCRYPTION_KEY', 'ENCRYPTION_IV'] })
+  .onCall((encryptedText) => {
+    const keyHex = process.env.ENCRYPTION_KEY;
+    const ivHex = process.env.ENCRYPTION_IV;
+    const decipher = crypto.createDecipheriv(
+      'aes-256-cbc',
+      Buffer.from(keyHex, 'hex'),
+      Buffer.from(ivHex, 'hex')
+    );
+    let plainText = decipher.update(encryptedText, 'hex', 'utf8');
+    plainText += decipher.final('utf8');
+    return plainText;
+  });
