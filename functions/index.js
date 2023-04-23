58cab448f600cf4651d58f50d5d19939aa167ae0
Jaden Bertino <jaden@bertinofamily.com>
2023-04-23 13:12:47 -0700

    create encrypt + decrypt functions

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,19 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const crypto = require('crypto');
+
+function encryptText(plainText, keyHex, ivHex) {
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(keyHex, 'hex'), Buffer.from(ivHex, 'hex'));
+  let encryptedText = cipher.update(plainText, 'utf8', 'hex');
+  encryptedText += cipher.final('hex');
+  return encryptedText
+}
+
+function decryptText(encryptedText, keyHex, ivHex) {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(keyHex, 'hex'), Buffer.from(ivHex, 'hex'));
+  let clearText = decipher.update(encryptedText, 'hex', 'utf8');
+  clearText += decipher.final('utf8');
+  return clearText;
+}
